44f2a8dd59d459e23fa7a3bb9d87a5f4fe669952
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-03-14 23:51:38 +0530

    Update home.tsx

--- frontend/app/(tabs)/home.tsx
+++ frontend/app/(tabs)/home.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function home() {
+export default function HomeScreen() {
   return (
-    <View>
-      <Text>home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Brain Booster</Text>
+      <Text style={styles.subtitle}>Boost your brain every day 🧠</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Start Daily Challenge</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#4CAF50",
+    padding: 15,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
