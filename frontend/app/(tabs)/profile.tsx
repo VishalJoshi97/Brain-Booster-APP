a00130464720d1dbab2ee6bc743e7c6e3f8a9837
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-03-15 23:39:43 +0530

    Update profile.tsx

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-export default function profile() {
+export default function ProfileScreen() {
   return (
-    <View>
-      <Text>profile</Text>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: "https://via.placeholder.com/120" }}
+        style={styles.avatar}
+      />
+
+      <Text style={styles.name}>Vishal</Text>
+      <Text>Level: Brain Master</Text>
+      <Text>Total Sessions: 45</Text>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  avatar: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    marginBottom: 15,
+  },
+  name: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
