2b0a6b057211d0a618ddad831a182b46bb5f3e5e
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-03-28 23:28:04 +0530

    Create useInterval.ts

---
+++ frontend/useInterval.ts
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from "react";
+
+export default function useInterval(callback: () => void, delay: number) {
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    if (delay === null) return;
+
+    const id = setInterval(() => {
+      savedCallback.current();
+    }, delay);
+
+    return () => clearInterval(id);
+  }, [delay]);
+}
